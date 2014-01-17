baab9678073efe9abbf67b2f4024224e51df97f4
Melissa Sondheimer <msondheimer@uchicago.edu>
2014-01-17 15:06:47 -0600

    weather

--- weather.rb
+++ weather.rb
@@ -3,10 +3,19 @@ require 'json'
 
 puts "Enter an address:"
 
-input = gets.gsub(" ","")
+input = gets.gsub(" ","+")
 address = input.rstrip
 
-json_data = open("http://api.openweathermap.org/data/2.5/weather?q=#{address}&units=imperial").read
+json_data = open("http://maps.googleapis.com/maps/api/geocode/json?address=#{address}&sensor=false").read
+data = JSON.parse(json_data)
+
+data1 = data["results"]
+
+lat = data1[0]['geometry']["location"]["lat"].round(2)
+lon = data1[0]['geometry']["location"]["lng"].round(2)
+
+json_data= open("http://api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{lon}&units=imperial").read
+
 data = JSON.parse(json_data)
 
 temperature = data["main"]["temp"]
